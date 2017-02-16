77952064ae19294e5f8e599fda4560da40dd7418
Rose <rosewhite901@hotmail.com>
2017-02-16 21:55:43 +1300

    animation started

---
+++ js/site.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#block1").animate({
+            width: "50vw"
+        });
+        $("#block2").animate({
+            left: "50vw"
+        });
+
+    }, 2000);
+
+})
