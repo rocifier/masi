458a8ffaf6303537095bfc260bc475447f74b26d
Rose <rosewhite901@hotmail.com>
2017-02-20 20:59:51 +1300

    step1,2,3

--- js/site.js
+++ js/site.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
+    // step 1
     setTimeout(function() {
         $("#block1").animate({
+            // step 2
             width: "50vw"
+        }, {
+            duration: 1000,
+            complete: function() {
+
+                // step 3
+                $("#block3").animate({
+                    top: "50vh"
+                }, {
+                    duration: 1000,
+                    complete: function() {
+
+                    }
+                });
+                $("#block2").animate({
+                    height: "50vh"
+                }, {
+                    duration: 1000,
+                    complete: function() {
+
+                    }
+                });
+            }
         });
         $("#block2").animate({
             left: "50vw"
-        });
+
+        }, { duration: 1000 });
 
     }, 2000);
+
 
 })
